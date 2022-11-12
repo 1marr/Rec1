f8d1bdab7151f42c403fb0bf1f6e349092370783
Mardinae Gary <mjgary21@students.desu.edu>
2022-11-11 22:39:28 -0500

    completed

--- cypher.py
+++ cypher.py
@@ -11,9 +11,30 @@ ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
 
 def encrypt(message, password):
     encrypted_message = ''
-    pass  # TODO replace this line with your code.
+    j = 0
+    for i in message:
+        if j == len(password):
+            j = 0
+        x = alphabet[(ordinal_value[i] + ordinal_value[password[j]]) % len(alphabet)]
+        encrypted_message += x
+        j += 1
+
+    return encrypted_message
 
 
 def decrypt(message, password):
     decrypted_message = ''
-    pass  # TODO replace this line with your code.
+    j = 0
+    for i in message:
+        if j == len(password):
+            j = 0
+        k = password[j % len(password)]
+        x = alphabet[(ordinal_value[i] - ordinal_value[k]) % len(alphabet)]
+        j += 1
+        decrypted_message += x
+
+    return decrypted_message
+
+
+print(ordinal_value)
+print(encrypt('This is my message', 'My#1Secret Password'))
